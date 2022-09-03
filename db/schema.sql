ed6ece373f2707d5c210460a0a840a2e6445e0b4
Ajalloh1 <mys4jalloh@gmail.com>
2022-09-02 22:56:31 -0400

    added data in schema file

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+-- creating database for employee. if exist drop if not create//
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+create table department (
+  id INT UNSIGNED AUTO_INCREMENT 
+  PRIMARY KEY,
+  d_name VARCHAR(30)
+);
+
+-- creating roles of employees with primary key
+create table role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) 
+  REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- data for employee informations
+create table employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  last_name VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) 
+  REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) 
+  REFERENCES employee(id) ON DELETE SET NULL
+);
