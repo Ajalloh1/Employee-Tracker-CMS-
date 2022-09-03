e8c7b7507103b21fcabd8c7dd7abf2cb8d22d5c3
Ajalloh1 <mys4jalloh@gmail.com>
2022-09-03 14:55:24 -0400

    dded in serverjs a sciiart logo

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
+// requiring the dependencies
 const inquirer = require('inquirer');
 const util = require('util');
 const mysql = require('mysql2');
 const fs = require('fs');
 const cTable = require('console.table');
+
+//creating logo using asciiart dependency conneting to the json file//
+const logoStamp = require('asciiart-logo');
+const config = require('./package.json');
+console.log(logoStamp(config).render());
+
+//connecting mysql with credentials//
+
+const connect = mysql.createConnect({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: 'Conakry1',
+    database: 'employee_db'
+
+});
