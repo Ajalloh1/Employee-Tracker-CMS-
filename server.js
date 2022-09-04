6447de3f7b9ffb25aaeec65d28c3c164518f1a22
Ajalloh1 <mys4jalloh@gmail.com>
2022-09-03 21:57:24 -0400

    added more prompts in serverjs

--- server.js
+++ server.js
@@ -40,15 +40,82 @@ function search() {
         .prompt({
             name: "options",
             type: "list of options",
-            message: "Please choose from the list below?",
+            message: "Please select from the list below to view?",
             choices: [
                 "View all employees",
-                "View all departments",
                 "View all roles",
-                "Add employee",
+                "View all departments",
                 "Add Department",
+                "Add employee",
                 "Add Role",
-                "Remove employee",
                 "Update employee role",
-                "Update employee manager"
+                "Update employee manager",
+                "Remove employee",
             ]
+        }).then(answers => {
+            // changing or switching statement//
+            switch (answers.action) {
+                case "all employees":
+
+                    byEmployees();
+                    runSearch();
+
+                    break;
+                //new case //
+                case "departments":
+
+                    byDepartment();
+                    runSearch();
+
+                    break;
+
+                case "all roles":
+
+                    byRole();
+                    runSearch();
+
+                    break;
+                //case for adding employees data input//
+                case "Add employee":
+                    inquirer
+                        .prompt([
+                            {
+                                name: "employees last name",
+                                type: "input",
+                                message: "What is the employee's last name?",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "you must enter employee's last name";
+                                }
+                            },
+                            {
+                                name: "employee first name",
+                                type: "input",
+                                message: "What is the employee's first name",
+                                validate: answer => {
+                                    if (answer !== "") {
+                                        return true;
+                                    }
+                                    return "you must enter employee's first name";
+                                }
+                            },
+                            {
+                                name: "department",
+                                type: "input",
+                                message: "please enter the id of role",
+
+                            },
+                            {
+                                name: "manager",
+                                type: "input",
+                                message: "Please enter manager id",
+                            }
+                        ]).then(answers => {
+
+                            addEmployee(answers.employeeFirst, answers.employeeLast, answers.department, answers.manager);
+                            runSearch();
+                        })
+
+                    break;
